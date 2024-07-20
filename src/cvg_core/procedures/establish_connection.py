9f99379192b46d258e44a091cf49cf3fa8047e2a
t-nician <dev@senpai.zone>
2024-07-20 13:19:26 -0400

    implemented SendReceiveProcedures to establish_connection.py

--- src/cvg_core/procedures/establish_connection.py
+++ src/cvg_core/procedures/establish_connection.py
@@ -3,8 +3,8 @@ from typing import Callable
 from cvg_core.objects.network_object.packet_object import PacketType, PacketObject
 from cvg_core.objects.network_object.connection_object import ConnectionType, ConnectionState, ConnectionObject
 
-from cvg_core.procedures.send_and_receive import receive_into_and_send, receive_and_send, send_and_receive, send, receive
-from cvg_core.procedures.crypto_send_and_receive import crypto_exchange, crypto_receive_into_and_send, crypto_receive_and_send, crypto_send_and_receive, crypto_send, crypto_receive
+from cvg_core.proper_procedures import SendReceiveProcedures
+from cvg_core.procedures.crypto_send_and_receive import crypto_exchange
 
 
 def __fab_granted(
@@ -38,14 +38,12 @@ def __fab_password(
 
 
 def __exchange_password(
-    scope_send_and_receive: Callable[
-        [ConnectionObject, PacketObject], PacketObject
-    ],
+    procedures: SendReceiveProcedures,
     connection: ConnectionObject,
     packet: PacketObject, 
     compare_password: bytes
 ) -> PacketObject:
-    received_password = scope_send_and_receive(
+    received_password: PacketObject = procedures.send_and_receive(
         connection,
         __fab_password(packet)
     )
@@ -60,27 +58,17 @@ def __exchange_password(
 def establish_connection(
     connection: ConnectionObject, 
     password: bytes | None = None
-):
-    scope_send_and_receive = send_and_receive
-    #scope_send, scope_receive = send, receive
-    
-    #scope_receive_and_send = receive_and_send
-    scope_receive_into_and_send = receive_into_and_send
-    
+):  
     if connection.encryption_enabled:
         crypto_exchange(connection)
-        
-        scope_send_and_receive = crypto_send_and_receive
-        #scope_send, scope_receive = crypto_send, crypto_receive
-        
-        #scope_receive_and_send = crypto_receive_and_send
-        scope_receive_into_and_send = crypto_receive_into_and_send
     
+    procedures = SendReceiveProcedures(connection)
+      
     if connection.type is ConnectionType.SERVER_TO_CLIENT:
         gateway_packet: PacketObject
         
         if password:
-            gateway_packet = scope_receive_into_and_send(
+            gateway_packet = procedures.receive_into_and_send(
                 connection,
                 PacketType.GATEWAY
             )(
@@ -88,11 +76,11 @@ def establish_connection(
                     packet=_,
                     compare_password=password,
                     connection=connection,
-                    scope_send_and_receive=scope_send_and_receive
+                    procedures=procedures
                 )
             )
         else:
-            gateway_packet = scope_receive_into_and_send(
+            gateway_packet = procedures.receive_into_and_send(
                 connection,
                 PacketType.GATEWAY
             )(__fab_granted)
@@ -100,13 +88,13 @@ def establish_connection(
         assert gateway_packet.type is PacketType.GRANTED
         
     elif connection.type is ConnectionType.CLIENT_TO_SERVER:
-        entry_response: PacketObject = scope_send_and_receive(
+        entry_response: PacketObject = procedures.send_and_receive(
             connection,
             PacketObject(b"", PacketType.GATEWAY)
         )
         
         if entry_response.type is PacketType.PASSWORD:            
-            password_response: PacketObject = scope_send_and_receive(
+            password_response: PacketObject = procedures.send_and_receive(
                 connection,
                 __fab_password(entry_response.id, password)
             )
