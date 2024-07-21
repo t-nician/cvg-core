b396a1444aa10678b545b4e9da50f9cdec4f8d3e
t-nician <dev@senpai.zone>
2024-07-21 17:29:02 -0400

    MOREEE. it's alot but will be easy for me to ID failures.

--- src/cvg_core/tests/__init__.py
+++ src/cvg_core/tests/__init__.py
@@ -1,4 +1,5 @@
-from cvg_core.tests import test_send, test_receive, test_stream, test_crypto, test_login, test_establish
+from cvg_core.tests import test_send, test_receive, test_stream, test_send_and_receive, test_login, test_establish
+from cvg_core.tests import test_crypto_send, test_crypto_receive, test_crypto_stream, test_crypto_send_and_receive, test_crypto_login, test_crypto_establish
 
 
 def start_tests():
